a4a98a9d19caae86ac2d07c5ce8c681c45c1178a
Jonathan-Xue <jonathanxue50@gmail.com>
2020-05-04 20:04:57 -0500

    Updates

---
+++ precache-manifest.33c26287a6e46a3c8c3df03ab5d60639.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e13c4a091653db68ea46283808e695",
+    "url": "/CourseAssign/index.html"
+  },
+  {
+    "revision": "007467628a380e891c47",
+    "url": "/CourseAssign/static/css/main.b3764a01.chunk.css"
+  },
+  {
+    "revision": "abe4b834aba64d0d8b45",
+    "url": "/CourseAssign/static/js/2.7ec9d8cb.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/CourseAssign/static/js/2.7ec9d8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007467628a380e891c47",
+    "url": "/CourseAssign/static/js/main.adf767af.chunk.js"
+  },
+  {
+    "revision": "fc77a2f34eecc975e68d",
+    "url": "/CourseAssign/static/js/runtime-main.457c463c.js"
+  },
+  {
+    "revision": "23c73952e6794f6e18589d8fbf7d3892",
+    "url": "/CourseAssign/static/media/background.23c73952.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CourseAssign/static/media/logo.5d5d9eef.svg"
+  }
+]);
